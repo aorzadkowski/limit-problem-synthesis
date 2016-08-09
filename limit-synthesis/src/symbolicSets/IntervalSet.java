f82933ade5be03eb91990462334b6d613917eb19
BlakeGlueck <bglueck@mail.bradley.edu>
2016-08-09 12:39:41 -0500

    Delete IntervalSet.java

--- limit-synthesis/src/symbolicSets/IntervalSet.java
+++
@@ -1,8 +0,0 @@
-package symbolicSets;
-
-public abstract class IntervalSet 
-{
-	public abstract boolean contains(double aDouble); //returns true if aDouble lies on this interval.
-	public abstract double getFrom();				//returns the double value _from, the starting point of every IntervalSet.
-	public abstract boolean equals(IntervalSet intSet);	//returns true if intSet has the same _from and _to and is the same IntervalSet type.
-}
